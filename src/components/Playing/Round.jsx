b21f22d9b35e07906f91eb9ac14fe643d167c460
Pickles <pickles@pop-os.localdomain>
2021-04-19 10:26:23 -0400

    finished refactoring

--- src/components/Playing/Round.jsx
+++ src/components/Playing/Round.jsx
@@ -1,23 +1,35 @@
-import {React, useState} from 'react';
+import {React} from 'react';
 
 const Round = ({round, roundMax, handleSettingsClick}) => {
-const [hovering, setHovering] = useState(false);
+// const [hovering, setHovering] = useState(false);
  
-return (hovering ? (
-      <div 
-        className='settings' 
-        onClick={() => handleSettingsClick()}
-        onMouseLeave={() => setHovering(false)}
-      >
-        Settings
-      </div>
-    )
-      : <div
-          className='round'
-          onMouseEnter={() => setHovering(true)}
-        >
-        Round: {round}/{roundMax}
-        </div>
-    )
+// TODO Settings show on hover
+// Todo waiting for settings feature to be added
+// return (hovering ? (
+//       <div 
+//         className='settings' 
+//         onClick={() => handleSettingsClick()}
+//         onMouseLeave={() => setHovering(false)}
+//       >
+//         Settings
+//       </div>
+//     )
+//       : <div
+//           className='round'
+//           onMouseEnter={() => setHovering(true)}
+//         >
+//         Round: {round}/{roundMax}
+//         </div>
+//     )
+// }
+
+  return (
+    <div
+      className='round'
+    >
+    Round: {round}/{roundMax}
+    </div>
+  )
 }
+
 export default Round;
